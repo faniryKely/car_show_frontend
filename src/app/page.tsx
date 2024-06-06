91854d581d61c0295002282bc2fc42fac9269958
= <=>
2024-06-06 10:53:44 +0300

    fix: make Landing page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import Footer from "@/components/footer/Footer";
+import LandingPage from "@/components/landingPage/LandingPage";
 import Header from "@/layouts/header/Header";
 
 export default function Homes(){
     return (
         <>
-        <h1>hm</h1>
-        </>
-    
+        <LandingPage/>
+        </>    
     )
 }
