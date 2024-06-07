723aa5232b70f4f93eea2696542a99ab97ddbc9a
Antenaina2002 <hei.antenaina@gmail.com>
2024-06-07 23:22:44 +0300

    chore: footer design

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,11 +1,43 @@
+import React from 'react';
 import './footer.css';
-export default function Footer () {
-return(
-    <div className='footer'>
-        <h1>RASSETA</h1>
-        <h1>CONTACT</h1>
-        <h1>FOOTER</h1>
-        <h1>BRANDS</h1>
+
+const socialLinks = [
+  { platform: 'Instagram', url: '#' },
+  { platform: 'Snapchat', url: '#' },
+  { platform: 'Twitter', url: '#' },
+  { platform: 'Facebook', url: '#' },
+];
+
+const footerContent = [
+  { label: 'Home', url: '#' },
+  { label: 'Services', url: '#' },
+  { label: 'About', url: '#' },
+  { label: 'Terms', url: '#' },
+  { label: 'Privacy Policy', url: '#' },
+];
+
+const Footer: React.FC = () => {
+  return (
+    <div className="footer-basic">
+      <footer>
+        <div className="social">
+          {socialLinks.map(({ platform, url }) => (
+            <a key={platform} href={url}>
+              <i className={`icon ion-social-${platform.toLowerCase().replace(' ', '-')}`}></i>
+            </a>
+          ))}
+        </div>
+        <ul className="list-inline">
+          {footerContent.map(({ label, url }) => (
+            <li key={label} className="list-inline-item">
+              <a href={url}>{label}</a>
+            </li>
+          ))}
+        </ul>
+        <p className="copyright">Company Name © 2024</p>
+      </footer>
     </div>
-)
-}
+  );
+};
+
+export default Footer;
