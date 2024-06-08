6ce629da370799747c46d68f0fd6650e2252a0f6
Faniry <faniryrandria08@gmail.com>
2024-06-08 07:37:01 +0300

    chore: modify headers

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,9 @@
-import Logo from "@/components/logo/Logo"
+
 import Navbar from "../Navbar/Navbar"
 import styles from "./header.module.css"
 export default function Header(){
     return (
     <div className={styles.container}>
-        <Logo/>
         <Navbar/>
     </div>
         
