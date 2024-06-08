479ed9928d8e0f7c72b39ee4765a96e1d19c75b6
Faniry <faniryrandria08@gmail.com>
2024-06-08 08:30:13 +0300

    chore : edit navbar

--- src/components/Navbar/link/Links.tsx
+++ src/components/Navbar/link/Links.tsx
@@ -1,34 +1,54 @@
-import Link from "next/link"
-import styles from "../navbar.module.css"
+import Link from "next/link";
+import styles from "../navbar.module.css";
+import { FaUser, FaSignOutAlt, FaCar } from "react-icons/fa";
 
-export default function Links(){
+export default function Links() {
     const links = [
         {
-            title:"Homes",
-            path:"/"
+            title: "Homes",
+            path: "/"
         },
         {
-            title:"Cars",
-            path:"/cars"
+            title: "Cars",
+            path: "/cars"
         },
         {
-            title:"Appointments",
-            path:"/appointments"
+            title: "Appointments",
+            path: "/appointments"
         },
         {
-            title:"Login",
-            path:"/login"
+            title: "Login",
+            path: "/login"
         },
         {
-            title:"Search car",
-            path:"/carSearch"
-        }
-    ]
+            title: "Search car",
+            path: "/carSearch"
+        },
+    ];
+
     return (
         <div className={styles.container}>
-            {links.map((link=>(
-                <Link href={link.path} key={link.title} className={styles.title}>{link.title}</Link>
-            )))}
+            <div className={styles.left}>
+                <Link href="/">
+                    <FaCar className={styles.icon} />
+                </Link>
+                <div className={styles.h1}>
+                     <h1>Rasseta</h1>
+                </div>
+                
+            </div>
+            <div className={styles.center}>
+                {links.map((link) => (
+                    <Link href={link.path} key={link.title} className={styles.title}>
+                        {link.title}
+                    </Link>
+                ))}
+            </div>
+            <div className={styles.right}>
+                <Link href="/login">
+                    <FaSignOutAlt className={styles.icon} />
+                </Link>
+            </div>
         </div>
-    )
+    );
 }
