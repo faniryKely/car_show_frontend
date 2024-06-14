ebca571f4d3e6d941a70d758eb2103e5069859b2
Faniry <faniryrandria08@gmail.com>
2024-06-14 19:42:13 +0300

    chore: modifying style for login and signup page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -54,10 +54,11 @@ const SignUpPage: React.FC = () => {
                         marginTop: 8,
                         display: 'flex',
                         flexDirection: 'column',
-                        alignItems: 'center'
+                        alignItems: 'center',
+                        backgroundImage : 'url("../../../public/Assets/milay-fotor-2024061413538.png")'
                     }}
                 >
-                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                    <Avatar sx={{ m: 1, bgcolor: '#A52A2A' }}>
                         <LockOutlinedIcon />
                     </Avatar>
                     <Typography component="h1" variant="h5">
@@ -124,7 +125,10 @@ const SignUpPage: React.FC = () => {
                             sx={{
                                 mt: 3,
                                 mb: 2,
-                                backgroundColor: 'rgba(126, 126, 129, 0.842)'
+                                backgroundColor: '#A52A2A',
+                                '&:hover': {
+                                    backgroundColor: '#B94A3B'
+                                }
                             }}
                         >
                             Sign Up
@@ -134,7 +138,10 @@ const SignUpPage: React.FC = () => {
                         variant="contained"
                         sx={{
                             width: '50%',
-                            backgroundColor: 'rgba(126, 126, 129, 0.842)'
+                            backgroundColor: '#A52A2A',
+                                '&:hover': {
+                                    backgroundColor: '#B94A3B'
+                             }
                         }}
                     >
                         <Link
@@ -150,7 +157,7 @@ const SignUpPage: React.FC = () => {
             <Box
                 sx={{
                     ml: 2,
-                    backgroundColor: 'rgba(126, 126, 129, 0.842)',
+                    backgroundColor: '#A52A2A',
                     borderRadius: '80%',
                     height: '45px',
                     width: '45px',
